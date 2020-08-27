a6a14d138b2a084b070048d842b3e17d33e9bad8
Tracy Mai <69124336+tracymai007@users.noreply.github.com>
2020-08-26 17:50:52 -0700

    pushing

--- Revassess/src/main/java/com/revature/tier1/SumOverArray.java
+++ Revassess/src/main/java/com/revature/tier1/SumOverArray.java
@@ -1,21 +1,29 @@
 package com.revature.tier1;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class SumOverArray {
-	
 
-	public static int IterateAndSum(int[] arr) {
-		return 0;
-	}
-	public static void main(String[] args) {  
-		
-	}
-		
-		public static int IterateAndSum1(int[] array) {
-			 int sum = 0; 
-			for (int i : array) { 
-			sum = sum + i;
-			 } return sum; 
 
-		
+		   public static void main(String args[]){
+		      System.out.println("Enter the required size of the array :: ");
+		      Scanner s = new Scanner(System.in);
+		      int size = s.nextInt();
+		      int myArray[] = new int [size];
+		      int sum = 0;
+		      System.out.println("Enter the elements of the array one by one ");
+
+		      for(int i=0; i<size; i++){
+		         myArray[i] = s.nextInt();
+		         sum = sum + myArray[i];
+		      }
+		      System.out.println("Elements of the array are: "+Arrays.toString(myArray));
+		      System.out.println("Sum of the elements of the array ::"+sum);
+		   }
+
+		public static Object IterateAndSum(int[] arr) {
+			// TODO Auto-generated method stub
+			return null;
 		}
-}
+		}
